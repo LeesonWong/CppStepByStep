c33ef35398cd53d058cf44c08877a0e17e906a65
LeesonWong <leesonwong_mouse@outlook.com>
2024-09-02 15:30:15 +0800

    note

--- part_01_1.cpp
+++ part_01_1.cpp
@@ -50,41 +50,4 @@ int main () {
     char32_t ch2 = 'd';
 
     cout << "ÄãºÃ" << endl;
-
-
-    // int i = 5;
-    // int& iR = i;
-    // int&& num = 5;
-    // int anoNum = num;
-    // // cout << typeid(i).name() << endl;
-    // // cout << typeid(iR).name() << endl;
-    // // cout << typeid(num).name() << endl;
-    // // cout << typeid(anoNum).name() << endl;
-    //
-    // auto & ai = i;
-    // auto && aii = i;
-    // auto && aiii = 50;
-    // // cout << typeid(ai).name() << endl;
-    // // cout << typeid(aii).name() << endl;
-    // // cout << typeid(aiii).name() << endl;
-    //
-    //
-    // int arr[5] {1, 2, 3, 4, 5};
-    // auto arrNext = arr;
-    // cout << typeid(arr).name() << endl;
-    // cout << typeid(arrNext).name() << endl;
-    //
-    // auto arr1 = {1};
-    // auto arr2 = {1, 2, 3, 4};
-    // cout << typeid(arr1).name() << endl;
-    // cout << typeid(arr2).name() << endl;
-    //
-    // auto s1{1};
-    // auto s2{1.1};
-    // auto s3{'c'};
-    // auto s4{"word"};
-    // cout << typeid(s1).name() << endl;
-    // cout << typeid(s2).name() << endl;
-    // cout << typeid(s3).name() << endl;
-    // cout << typeid(s4).name() << endl;
 }
--- part_01_3.cpp
+++ part_01_3.cpp
@@ -11,6 +11,50 @@ public:
     string name;
 };
 
+// short 2
+struct MySize0 {
+    char c1;
+    short s1;
+};
+
+// short 2
+struct MySize1 {
+    bool b1;
+    short s1;
+};
+
+// short 2
+struct MySize2 {
+    short s1;
+};
+
+// int 4
+struct MySize3 {
+    int i1;
+};
+
+// long 4
+struct MySize4 {
+    long l1;
+};
+
+// long long 8
+struct MySize5 {
+    long long ll1;
+    bool b1;
+    bool b2;
+    bool b3;
+    bool b4;
+    bool b5;
+    int i1;
+};
+
+// double 8
+struct MySize6 {
+    double d1;
+    // float f1;
+};
+
 // 位域 对复杂成员声明位域会导致程序异常，对于基础类型有些用
 // 一般只在特别需要节省内存的场景使用，例如嵌入式系统和低级系统编程
 int main() {
@@ -19,7 +63,25 @@ int main() {
     s.height = 100;
     s.name = "Hello World";
 
-    cout << s.age << endl;
-    cout << s.height << endl;
-    cout << s.name << endl;
+    // cout << s.age << endl;
+    // cout << s.height << endl;
+    // cout << s.name << endl;
+
+    MySize0 size0;
+    MySize1 size1;
+    MySize2 size2;
+    MySize3 size3;
+    MySize4 size4;
+    MySize5 size5;
+    MySize6 size6;
+
+    PRINT_L(size0);
+    PRINT_L(size1);
+    PRINT_L(size2);
+    PRINT_L(size3);
+    PRINT_L(size4);
+    PRINT_L(size5);
+    PRINT_L(size6);
+
+    return 0;
 }
